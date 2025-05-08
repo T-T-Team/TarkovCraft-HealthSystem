eb88e3c6c54e5c53e47c3faeca748f6eff957397
Toma <novotny.tom96@gmail.com>
2025-05-08 21:56:32 +0200

    Config localization

--- src/main/java/tnt/tarkovcraft/medsystem/client/config/HealthOverlayConfiguration.java
+++ src/main/java/tnt/tarkovcraft/medsystem/client/config/HealthOverlayConfiguration.java
@@ -11,19 +11,23 @@ public class HealthOverlayConfiguration extends ConfigurableOverlay {
     @Configurable.DecimalRange(min = 0.5F, max = 2.5F)
     @Configurable.Gui.Slider
     @Configurable.Gui.NumberFormat("0.0#")
+    @Configurable.Comment(localize = true, value = "Health overlay scale")
     public float scale = 1.0F;
 
     @Configurable
     @Configurable.Range(min = 0, max = 255)
     @Configurable.Gui.Slider
+    @Configurable.Comment(localize = true, value = "Health overlay transparency")
     public int transparency = 136;
 
     @Configurable
     @Configurable.Gui.ColorValue
+    @Configurable.Comment(localize = true, value = "Overlay color for dead body parts")
     public String deadLimbColor = "#444444";
 
     @Configurable
     @Configurable.Gui.ColorValue
+    @Configurable.Comment(localize = true, value = "Color schema used for color blending of body part overlay based on the part health")
     public String[] colorSchema = { "#00FF00", "#FFFF00", "#FF0000" };
 
     public HealthOverlayConfiguration(boolean enabled, HorizontalAlignment horizontalAlignment, VerticalAlignment verticalAlignment, int x, int y) {
--- src/main/java/tnt/tarkovcraft/medsystem/common/config/MedSystemConfig.java
+++ src/main/java/tnt/tarkovcraft/medsystem/common/config/MedSystemConfig.java
@@ -8,26 +8,31 @@ import tnt.tarkovcraft.medsystem.MedicalSystem;
 public final class MedSystemConfig {
 
     @Configurable
+    @Configurable.Comment(localize = true, value = "Includes all equipped armors for damage reduction calculation")
     public boolean simpleArmorCalculation = false;
 
     @Configurable
     @Configurable.DecimalRange(min = 0.15, max = 3.0)
     @Configurable.Gui.Slider
     @Configurable.Gui.NumberFormat("0.00#")
+    @Configurable.Comment(localize = true, value = "Damage scale for explosions")
     public float explosionDamageScale = 0.6F;
 
     @Configurable
     @Configurable.DecimalRange(min = 0, max = 1.0)
     @Configurable.Gui.Slider
     @Configurable.Gui.NumberFormat("0.00#")
+    @Configurable.Comment(localize = true, value = "Losing limb has small chance to cause immediate death")
     public float limbLossDeathCauseChance = 0.05F;
 
     @Configurable
+    @Configurable.Comment(localize = true, value = "Health will be primarily recovered into vital parts")
     public boolean prioritizeVitalHealing = true;
 
     @Configurable
     @Configurable.DecimalRange(min = 0, max = 1.0)
     @Configurable.Gui.Slider
     @Configurable.Gui.NumberFormat("0.0##")
+    @Configurable.Comment(localize = true, value = "Threshold for prioritized vital body part health recovery")
     public float vitalBodyPartHealthTrigger = 0.75F;
 }
