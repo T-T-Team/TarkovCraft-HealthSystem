e3c0a539655a690eee607a45d184fe47ef418a55
Toma <novotny.tom96@gmail.com>
2025-08-28 18:54:36 +0200

    Remove unused code

--- src/main/java/tnt/tarkovcraft/medsystem/common/health/BodyPartDisplay.java
+++ src/main/java/tnt/tarkovcraft/medsystem/common/health/BodyPartDisplay.java
@@ -2,15 +2,12 @@ package tnt.tarkovcraft.medsystem.common.health;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.phys.Vec2;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
-import tnt.tarkovcraft.core.util.ObjectCache;
 
 public record BodyPartDisplay(String source, Vec2 pos, Vec2 size) {
 
-    public static final ObjectCache<String, Component> DISPLAY_NAME = new ObjectCache<>(name -> Component.translatable("label.tarkovcraft.bodypart." + name));
     public static final Codec<BodyPartDisplay> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.STRING.fieldOf("source").forGetter(BodyPartDisplay::source),
             Vec2.CODEC.fieldOf("pos").forGetter(BodyPartDisplay::pos),
@@ -26,9 +23,5 @@ public record BodyPartDisplay(String source, Vec2 pos, Vec2 size) {
                 sizeX,
                 sizeY
         );
-    }
-
-    public Component getDisplayName() {
-        return DISPLAY_NAME.get(this.source);
     }
 }
