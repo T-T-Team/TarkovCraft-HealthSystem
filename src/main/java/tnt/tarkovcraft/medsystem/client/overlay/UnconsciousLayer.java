81a2576aaa839b195c63d96dcea6f3d0380b106a
Toma <novotny.tom96@gmail.com>
2025-10-28 18:11:29 +0100

    Added rescue timer to HUD

--- src/main/java/tnt/tarkovcraft/medsystem/client/overlay/UnconsciousLayer.java
+++ src/main/java/tnt/tarkovcraft/medsystem/client/overlay/UnconsciousLayer.java
@@ -10,6 +10,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.client.gui.GuiLayer;
+import tnt.tarkovcraft.core.client.screen.ColorPalette;
+import tnt.tarkovcraft.core.common.data.duration.Duration;
+import tnt.tarkovcraft.core.common.data.duration.DurationFormats;
 import tnt.tarkovcraft.medsystem.MedicalSystem;
 import tnt.tarkovcraft.medsystem.common.status.BloodData;
 import tnt.tarkovcraft.medsystem.common.status.BloodSystem;
@@ -39,7 +42,14 @@ public class UnconsciousLayer implements GuiLayer {
             for (int i = 0; i < lines.size(); i++) {
                 FormattedCharSequence line = lines.get(i);
                 int textWidth = font.width(line);
-                guiGraphics.drawString(font, line, (window.getGuiScaledWidth() - textWidth) / 2, 30 + i * 11, 0xFFFFFFFF, true);
+                guiGraphics.drawString(font, line, (window.getGuiScaledWidth() - textWidth) / 2, 30 + i * 11, ColorPalette.WHITE, true);
+            }
+
+            if (info.causesDeath()) {
+                Duration timer = Duration.ticks(data.getRemainingUnconsciousTime());
+                Component text = timer.format(DurationFormats.LONG_NAME);
+                int textWidth = font.width(text);
+                guiGraphics.drawString(font, text, (window.getGuiScaledWidth() - textWidth) / 2, 30 + (lines.size() + 1) * 11, ColorPalette.WHITE, true);
             }
         }
     }
