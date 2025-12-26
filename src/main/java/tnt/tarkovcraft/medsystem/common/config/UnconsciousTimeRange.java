f2039b46da203b6c544d3835a17c2944b292cb98
Toma <novotny.tom96@gmail.com>
2025-12-26 13:44:25 +0100

    Configurable unconscious durations

---
+++ src/main/java/tnt/tarkovcraft/medsystem/common/config/UnconsciousTimeRange.java
@@ -0,0 +1,26 @@
+package tnt.tarkovcraft.medsystem.common.config;
+
+import dev.toma.configuration.config.Configurable;
+import net.minecraft.util.RandomSource;
+
+public final class UnconsciousTimeRange {
+
+    @Configurable
+    @Configurable.Range(min = 0)
+    public int minDuration;
+
+    @Configurable
+    @Configurable.Range(min = 0)
+    public int maxDuration;
+
+    public UnconsciousTimeRange(int minDuration, int maxDuration) {
+        this.minDuration = minDuration;
+        this.maxDuration = maxDuration;
+    }
+
+    public int getDurationInSeconds(RandomSource random) {
+        int min = Math.min(this.minDuration, this.maxDuration);
+        int bound = Math.max(this.minDuration, this.maxDuration) - min;
+        return 20 * (bound > 0 ? min + random.nextInt(bound) : min);
+    }
+}
