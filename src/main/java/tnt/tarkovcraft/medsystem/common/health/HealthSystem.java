f73649771d476f96b09706176e12fe5bf44fa606
Toma <novotny.tom96@gmail.com>
2025-12-27 15:11:44 +0100

    Fix leftover issues after bloodloss effect changes

--- src/main/java/tnt/tarkovcraft/medsystem/common/health/HealthSystem.java
+++ src/main/java/tnt/tarkovcraft/medsystem/common/health/HealthSystem.java
@@ -106,6 +106,13 @@ public final class HealthSystem extends SimpleJsonResourceReloadListener {
         if (map.hasEffect(MedSystemTags.StatusEffects.MOVEMENT_RESTRICTING)) {
             return true;
         }
+        if (BloodSystem.hasBloodDataIntegration(entity)) {
+            BloodData data = BloodSystem.getBloodData(entity);
+            BloodStatus status = BloodStatus.fromBloodLevelPercentage(data.getBloodVolumePercentage());
+            if (status.isSameOrBelow(BloodStatus.MODERATE_BLOOD_LOSS)) {
+                return true;
+            }
+        }
         Stream<Limb> parts = healthContainer.getLimbsAsStream();
         return parts.anyMatch(HealthSystem::isMovementRestrictingPart);
     }
