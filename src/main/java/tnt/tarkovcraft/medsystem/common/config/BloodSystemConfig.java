95d83765abb64f2d247573fe423afc7f4470bdf4
Toma <novotny.tom96@gmail.com>
2026-04-02 15:05:45 +0200

    Added option to show current blood level in health UI

--- src/main/java/tnt/tarkovcraft/medsystem/common/config/BloodSystemConfig.java
+++ src/main/java/tnt/tarkovcraft/medsystem/common/config/BloodSystemConfig.java
@@ -11,6 +11,11 @@ public final class BloodSystemConfig {
     public boolean useBloodSystem = true;
 
     @Configurable
+    @Configurable.Synchronized
+    @Configurable.Comment("Enables blood level display in health UI")
+    public boolean showBloodLevel = false;
+
+    @Configurable
     @Configurable.Comment({
             "Defines handling of unconscious bleed out stage when the affected player has too low blood level to wake up on their own",
             "When disabled, bleed out damage will be applied immediately after losing ability to wake up"
