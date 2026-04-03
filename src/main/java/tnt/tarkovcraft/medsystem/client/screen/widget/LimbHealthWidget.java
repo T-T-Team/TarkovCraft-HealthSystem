d3fa497f5d063ea9c97171dfd084aabf4aa851a3
Toma <novotny.tom96@gmail.com>
2026-04-03 13:08:51 +0200

    Added painful label for pain causing status effects

--- src/main/java/tnt/tarkovcraft/medsystem/client/screen/widget/LimbHealthWidget.java
+++ src/main/java/tnt/tarkovcraft/medsystem/client/screen/widget/LimbHealthWidget.java
@@ -25,6 +25,7 @@ import tnt.tarkovcraft.medsystem.common.effect.StatusEffect;
 import tnt.tarkovcraft.medsystem.common.effect.StatusEffectType;
 import tnt.tarkovcraft.medsystem.common.effect.util.EffectVisibility;
 import tnt.tarkovcraft.medsystem.common.health.Limb;
+import tnt.tarkovcraft.medsystem.common.init.MedSystemTags;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -143,6 +144,9 @@ public class LimbHealthWidget extends AbstractWidget {
                     effect.addAdditionalInfo(tooltip::add);
                     if (effect.hasVisibleDuration() && !effect.isInfinite()) {
                         tooltip.add(StatusEffect.getDurationLabel(effect.getDuration()));
+                    }
+                    if (type.is(MedSystemTags.StatusEffects.IS_PAIN_CAUSING)) {
+                        tooltip.add(StatusEffect.PAINFUL_LABEL);
                     }
                     graphics.setTooltipForNextFrame(this.font, tooltip, Optional.empty(), mouseX, mouseY);
                 }
