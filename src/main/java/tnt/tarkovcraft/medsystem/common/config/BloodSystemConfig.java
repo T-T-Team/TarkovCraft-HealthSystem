e61126ba71a33f89740f70bafd3c88c17f3256cd
Toma <novotny.tom96@gmail.com>
2026-04-03 12:53:55 +0200

    Added option to show current blood type in health UI

--- src/main/java/tnt/tarkovcraft/medsystem/common/config/BloodSystemConfig.java
+++ src/main/java/tnt/tarkovcraft/medsystem/common/config/BloodSystemConfig.java
@@ -16,6 +16,11 @@ public final class BloodSystemConfig {
     public boolean showBloodLevel = false;
 
     @Configurable
+    @Configurable.Synchronized
+    @Configurable.Comment("Enables blood type display in health UI")
+    public boolean showBloodType = true;
+
+    @Configurable
     @Configurable.Comment({
             "Defines handling of unconscious bleed out stage when the affected player has too low blood level to wake up on their own",
             "When disabled, bleed out damage will be applied immediately after losing ability to wake up"
