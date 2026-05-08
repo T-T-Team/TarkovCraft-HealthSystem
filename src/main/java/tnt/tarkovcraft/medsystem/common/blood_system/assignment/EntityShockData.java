5530565610d3bbe7ffaf9ee69e531cacd2e0aa49
Toma <novotny.tom96@gmail.com>
2026-05-08 14:05:04 +0200

    Extended unconscious state from pain shock, added wake up threshold for shock

--- src/main/java/tnt/tarkovcraft/medsystem/common/blood_system/assignment/EntityShockData.java
+++ src/main/java/tnt/tarkovcraft/medsystem/common/blood_system/assignment/EntityShockData.java
@@ -4,18 +4,21 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.ExtraCodecs;
 
-public record EntityShockData(float receptionMultiplier, float recoveryRate, float inShockRecoveryMultiplier, float unconsciousThreshold) {
+public record EntityShockData(float receptionMultiplier, float recoveryRate, float inShockRecoveryMultiplier, float unconsciousThreshold, float unconsciousWakeUpThreshold) {
 
-    public static final EntityShockData DEFAULT = new EntityShockData(1.0F, 0.0005F, 0.1F, 0.75F);
+    public static final EntityShockData DEFAULT = new EntityShockData(1.0F, 0.0005F, 0.1F, 0.75F, 0.15F);
 
     public static final Codec<EntityShockData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             ExtraCodecs.NON_NEGATIVE_FLOAT.optionalFieldOf("reception_multiplier", DEFAULT.receptionMultiplier).forGetter(EntityShockData::receptionMultiplier),
             ExtraCodecs.NON_NEGATIVE_FLOAT.optionalFieldOf("recovery_rate", DEFAULT.recoveryRate).forGetter(EntityShockData::recoveryRate),
             ExtraCodecs.NON_NEGATIVE_FLOAT.optionalFieldOf("in_shock_recovery_multiplier", DEFAULT.inShockRecoveryMultiplier).forGetter(EntityShockData::inShockRecoveryMultiplier),
-            Codec.floatRange(0.0F, 1.0F).optionalFieldOf("unconscious_threshold", DEFAULT.unconsciousThreshold).forGetter(EntityShockData::unconsciousThreshold)
+            Codec.floatRange(0.0F, 1.0F).optionalFieldOf("unconscious_threshold", DEFAULT.unconsciousThreshold).forGetter(EntityShockData::unconsciousThreshold),
+            Codec.floatRange(0.0F, 1.0F).optionalFieldOf("unconscious_wake_up_threshold", DEFAULT.unconsciousWakeUpThreshold).forGetter(EntityShockData::unconsciousWakeUpThreshold)
     ).apply(instance, EntityShockData::new));
 
-    public boolean isUnconscious(float value) {
-        return value >= this.unconsciousThreshold;
+    public boolean isUnconscious(boolean unconscious, float value) {
+        return unconscious
+                ? value > this.unconsciousWakeUpThreshold
+                : value >= this.unconsciousThreshold;
     }
 }
