3929b327fffc79ad24743c0cea7d18c49c6d2d88
Toma <novotny.tom96@gmail.com>
2026-08-09 15:50:52 +0200

    update dependencies, integration with new features

--- src/main/java/tnt/tarkovcraft/medsystem/common/init/MedSystemSkillEvents.java
+++ src/main/java/tnt/tarkovcraft/medsystem/common/init/MedSystemSkillEvents.java
@@ -3,15 +3,15 @@ package tnt.tarkovcraft.medsystem.common.init;
 import net.minecraft.core.Holder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import tnt.tarkovcraft.core.common.init.CoreRegistries;
-import tnt.tarkovcraft.core.common.skill.tracker.SkillTriggerEvent;
+import tnt.tarkovcraft.core.common.skill.trigger.SkillTrigger;
 import tnt.tarkovcraft.medsystem.api.MedSystemConstants;
 
 public final class MedSystemSkillEvents {
 
-    public static final DeferredRegister<SkillTriggerEvent> REGISTRY = DeferredRegister.create(CoreRegistries.Keys.SKILL_TRIGGER_EVENT, MedSystemConstants.MOD_ID);
+    public static final DeferredRegister<SkillTrigger> REGISTRY = DeferredRegister.create(CoreRegistries.Keys.SKILL_TRIGGER_EVENT, MedSystemConstants.MOD_ID);
 
-    public static final Holder<SkillTriggerEvent> ARMOR_USE = REGISTRY.register("armor_use", SkillTriggerEvent::new);
-    public static final Holder<SkillTriggerEvent> DAMAGE_TAKEN = REGISTRY.register("damage_taken", SkillTriggerEvent::new);
-    public static final Holder<SkillTriggerEvent> HEALING_USED = REGISTRY.register("healing_used", SkillTriggerEvent::new);
-    public static final Holder<SkillTriggerEvent> LIMB_FIXED = REGISTRY.register("limb_fixed", SkillTriggerEvent::new);
+    public static final Holder<SkillTrigger> ARMOR_USE = REGISTRY.register("armor_use", SkillTrigger::new);
+    public static final Holder<SkillTrigger> DAMAGE_TAKEN = REGISTRY.register("damage_taken", SkillTrigger::new);
+    public static final Holder<SkillTrigger> HEALING_USED = REGISTRY.register("healing_used", SkillTrigger::new);
+    public static final Holder<SkillTrigger> LIMB_FIXED = REGISTRY.register("limb_fixed", SkillTrigger::new);
 }
