9a4ddce57b90c6ae060b352d0caaea0cea03e8dc
Toma <novotny.tom96@gmail.com>
2026-08-24 18:35:01 +0200

    Fixed crash with some modded explosions

---
+++ src/main/java/tnt/tarkovcraft/medsystem/common/damage/condition/IsExplosionCondition.java
@@ -0,0 +1,35 @@
+package tnt.tarkovcraft.medsystem.common.damage.condition;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
+import tnt.tarkovcraft.medsystem.common.health.calc.HitCalculationContext;
+
+import java.util.Optional;
+
+public final class IsExplosionCondition implements DamageCondition {
+
+    private static final IsExplosionCondition INSTANCE = new IsExplosionCondition();
+    public static final MapCodec<IsExplosionCondition> CODEC = MapCodec.unit(INSTANCE);
+
+    public static Optional<Vec3> resolveExplosionPosition(HitCalculationContext ctx) {
+        if (ctx.hasDamagePosition()) {
+            return Optional.of(ctx.source().getSourcePosition().add(0, 0.5, 0));
+        }
+        Entity sourceEntity = ctx.getProjectile();
+        if (sourceEntity != null) {
+            return Optional.of(sourceEntity.getBoundingBox().getCenter());
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean test(HitCalculationContext hitCalculationContext) {
+        return resolveExplosionPosition(hitCalculationContext).isPresent();
+    }
+
+    @Override
+    public MapCodec<? extends DamageCondition> codec() {
+        return CODEC;
+    }
+}
